8c289dc686b530024f61a50d36b73b7f4d3502f9
abhishek572003 <abhishektiwari572003@gmail.com>
2023-06-24 20:47:46 +0530

    1st commit

---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by STUDENTS OF SIES GST | CSE (IOT) BRANCH |
+ABHISHEK T, SHASHANK K, ABHINAV T, DHRUV M |
+ATTENDANCE MANAGEMENT SYSTEM
+            </span>
+          </div>
+        </div>
+      </footer>
